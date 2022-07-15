fcc1ae673c08300f8d4112197bfe7c8466f19059
paulomoraiiis <paulodevmorais@gmail.com>
2022-07-15 16:03:35 -0300

    Criando tabelas

---
+++ database/migrations/2022_07_15_175653_create_produtos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->UnsignedBigInteger('cidade_id');
+            $table->integer('cod');
+            $table->string('nome');
+            $table->decimal('valor', 2);
+            $table->integer('estoque');
+            $table->timestamps();
+            $table->foreign('cidade_id')->references('id')->on('cidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
